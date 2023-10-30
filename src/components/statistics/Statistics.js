4f4d4d6022510880d0142b4c7ad18455781a2054
Yuriy Volkov <sir.volkov@outlook.com>
2023-10-30 17:46:06 +0200

    upd : statistics refact

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,18 +1,26 @@
-import { StatListEl } from './StatListEl';
+import { MdOutlinePercent } from 'react-icons/md';
 
 const StatListTitle = ({ title }) => {
   return <h2 className="title">{title}</h2>;
 };
 
+const StatListEl = ({ stats: { label, percentage } }) => {
+  return (
+    <li className="item">
+      <span className="label">{label}</span>
+      <span className="percentage">
+        {percentage}
+        <MdOutlinePercent />
+      </span>
+    </li>
+  );
+};
+
 const StatList = ({ stats }) => {
   return (
-    <ul>
+    <ul className="stat-list">
       {stats.map(data => {
-        return (
-          <li key={data.id}>
-            <StatListEl stats={data} />
-          </li>
-        );
+        return <StatListEl key={data.id} stats={data} />;
       })}
     </ul>
   );
